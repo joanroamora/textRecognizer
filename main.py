99ba1db05190c8ac333f76702cb0dbed40ea0308
joanroamora <41650757+joanroamora@users.noreply.github.com>
2022-03-23 15:35:34 -0500

    Code optimization

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from pytesseract import *
 ##Research for the Tesseract Software installation directory
 pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-
+##Remember that the image file must be stored in the main.py directory. 
+##Note the spelling of the image file name and replace the values in the following line of code. 
 img = Image.open("image.jpg")
 
+##The information becomes a string variable.
 devResult = pytesseract.image_to_string(img)
 
 print(devResult)
